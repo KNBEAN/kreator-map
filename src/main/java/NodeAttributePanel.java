34f94da5e28fb74a815bbbb282d03ac5e6334197
Piotrek1697 <30784285+Piotrek1697@users.noreply.github.com>
2019-03-11 21:59:50 +0100

    Collapsing panell added

--- src/main/java/NodeAttributePanel.java
+++ src/main/java/NodeAttributePanel.java
@@ -2,6 +2,7 @@ import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.uiDesigner.core.Spacer;
 import org.graphstream.graph.Node;
+import org.jdesktop.swingx.JXCollapsiblePane;
 
 import javax.swing.*;
 import java.awt.*;
@@ -22,6 +23,8 @@ public class NodeAttributePanel extends JPanel {
     private JTextArea locationDescriptionTextArea;
     private JTextField locationTagTextField;
     private JButton addAttributesButton;
+    private JPanel nodePanel;
+    private JButton toggleButton;
 
     private Node nodeClicked;
 
@@ -29,7 +32,10 @@ public class NodeAttributePanel extends JPanel {
         $$$setupUI$$$();
         hardToReachComboBox.addItem("true");
         hardToReachComboBox.addItem("false");
+        addListeners();
+    }
 
+    private void addListeners() {
         addAttributesButton.addActionListener(e -> {
 
             int floor = 0;
@@ -38,13 +44,13 @@ public class NodeAttributePanel extends JPanel {
             int locationID = 0;
             boolean hardToReach = false;
 
-            try{
+            try {
                 floor = Integer.parseInt(floorTextField.getText());
                 x = Float.parseFloat(xTextField.getText());
                 y = Float.parseFloat(yTextField.getText());
                 locationID = Integer.parseInt(locationIDTextField.getText());
                 hardToReach = Boolean.parseBoolean((String) hardToReachComboBox.getSelectedItem());
-            } catch (NumberFormatException ex){
+            } catch (NumberFormatException ex) {
                 JOptionPane.showMessageDialog(getParent(),
                         "Check if in all text fields are right type values",
                         "Error",
@@ -55,14 +61,27 @@ public class NodeAttributePanel extends JPanel {
             String locationDescription = locationDescriptionTextArea.getText();
             String locationTag = locationTagTextField.getText();
 
-            nodeClicked.setAttribute("floor",floor);
-            nodeClicked.setAttribute("x",x);
-            nodeClicked.setAttribute("y",y);
-            nodeClicked.setAttribute("locationID",locationID);
-            nodeClicked.setAttribute("hardToReach",hardToReach);
-            nodeClicked.setAttribute("locationName",locationName);
-            nodeClicked.setAttribute("locationDescription",locationDescription);
-            nodeClicked.setAttribute("locationTag",locationTag);
+            if (nodeClicked != null) {
+                nodeClicked.setAttribute("floor", floor);
+                nodeClicked.setAttribute("x", x);
+                nodeClicked.setAttribute("y", y);
+                nodeClicked.setAttribute("locationID", locationID);
+                nodeClicked.setAttribute("hardToReach", hardToReach);
+                nodeClicked.setAttribute("locationName", locationName);
+                nodeClicked.setAttribute("locationDescription", locationDescription);
+                nodeClicked.setAttribute("locationTag", locationTag);
+            }
+        });
+
+        Action toggleAction = nodePanel.getActionMap().get(JXCollapsiblePane.TOGGLE_ACTION);
+        toggleAction.putValue(JXCollapsiblePane.COLLAPSE_ICON, UIManager.getIcon("Tree.expandedIcon"));
+        toggleAction.putValue(JXCollapsiblePane.EXPAND_ICON, UIManager.getIcon("Tree.collapsedIcon"));
+
+        toggleButton.addActionListener(e -> {
+            if (((JXCollapsiblePane) nodePanel).isCollapsed())
+                toggleButton.setText("Hide Node Panel");
+            else
+                toggleButton.setText("Show Node Panel");
 
         });
     }
@@ -105,11 +124,14 @@ public class NodeAttributePanel extends JPanel {
      * @noinspection ALL
      */
     private void $$$setupUI$$$() {
+        createUIComponents();
         mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayoutManager(7, 1, new Insets(0, 0, 0, 0), -1, -1));
+        mainPanel.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
+        nodePanel.setLayout(new GridLayoutManager(7, 2, new Insets(0, 0, 0, 0), -1, -1));
+        mainPanel.add(nodePanel, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         basicInfoPanel = new JPanel();
         basicInfoPanel.setLayout(new GridLayoutManager(1, 5, new Insets(0, 0, 0, 0), -1, -1));
-        mainPanel.add(basicInfoPanel, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        nodePanel.add(basicInfoPanel, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         nodeIDLabel = new JLabel();
         nodeIDLabel.setText("");
         basicInfoPanel.add(nodeIDLabel, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(30, -1), null, 0, false));
@@ -123,9 +145,11 @@ public class NodeAttributePanel extends JPanel {
         basicInfoPanel.add(label2, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer1 = new Spacer();
         basicInfoPanel.add(spacer1, new GridConstraints(0, 4, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        final Spacer spacer2 = new Spacer();
+        nodePanel.add(spacer2, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         coordsInfoPanel = new JPanel();
         coordsInfoPanel.setLayout(new GridLayoutManager(1, 6, new Insets(0, 0, 0, 0), -1, -1));
-        mainPanel.add(coordsInfoPanel, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        nodePanel.add(coordsInfoPanel, new GridConstraints(3, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         final JLabel label3 = new JLabel();
         label3.setText("x:");
         coordsInfoPanel.add(label3, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
@@ -141,22 +165,17 @@ public class NodeAttributePanel extends JPanel {
         coordsInfoPanel.add(label5, new GridConstraints(0, 4, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         hardToReachComboBox = new JComboBox();
         coordsInfoPanel.add(hardToReachComboBox, new GridConstraints(0, 5, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-        final JLabel label6 = new JLabel();
-        Font label6Font = this.$$$getFont$$$(null, Font.BOLD, 12, label6.getFont());
-        if (label6Font != null) label6.setFont(label6Font);
-        label6.setText("Coordinates");
-        mainPanel.add(label6, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         locationInfoPanel = new JPanel();
         locationInfoPanel.setLayout(new GridLayoutManager(3, 5, new Insets(0, 0, 0, 0), -1, -1));
-        mainPanel.add(locationInfoPanel, new GridConstraints(5, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-        final JLabel label7 = new JLabel();
-        label7.setText("ID");
-        locationInfoPanel.add(label7, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        nodePanel.add(locationInfoPanel, new GridConstraints(5, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        final JLabel label6 = new JLabel();
+        label6.setText("ID");
+        locationInfoPanel.add(label6, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         locationIDTextField = new JTextField();
         locationInfoPanel.add(locationIDTextField, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(50, -1), null, 0, false));
-        final JLabel label8 = new JLabel();
-        label8.setText("Location name");
-        locationInfoPanel.add(label8, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label7 = new JLabel();
+        label7.setText("Location name");
+        locationInfoPanel.add(label7, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         locationNameTextField = new JTextField();
         locationInfoPanel.add(locationNameTextField, new GridConstraints(0, 3, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(50, -1), null, 0, false));
         locationDescriptionTextArea = new JTextArea();
@@ -164,31 +183,42 @@ public class NodeAttributePanel extends JPanel {
         if (locationDescriptionTextAreaFont != null)
             locationDescriptionTextArea.setFont(locationDescriptionTextAreaFont);
         locationInfoPanel.add(locationDescriptionTextArea, new GridConstraints(1, 2, 1, 3, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
+        final JLabel label8 = new JLabel();
+        label8.setText("Location description");
+        locationInfoPanel.add(label8, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JLabel label9 = new JLabel();
-        label9.setText("Location description");
-        locationInfoPanel.add(label9, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        final JLabel label10 = new JLabel();
-        label10.setText("Location tag");
-        locationInfoPanel.add(label10, new GridConstraints(2, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        label9.setText("Location tag");
+        locationInfoPanel.add(label9, new GridConstraints(2, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         locationTagTextField = new JTextField();
         locationInfoPanel.add(locationTagTextField, new GridConstraints(2, 2, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-        final Spacer spacer2 = new Spacer();
-        locationInfoPanel.add(spacer2, new GridConstraints(2, 4, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        final Spacer spacer3 = new Spacer();
+        locationInfoPanel.add(spacer3, new GridConstraints(2, 4, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        addAttributesButton = new JButton();
+        addAttributesButton.setText("Add attributes");
+        nodePanel.add(addAttributesButton, new GridConstraints(6, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label10 = new JLabel();
+        Font label10Font = this.$$$getFont$$$(null, Font.BOLD, 14, label10.getFont());
+        if (label10Font != null) label10.setFont(label10Font);
+        label10.setHorizontalAlignment(10);
+        label10.setHorizontalTextPosition(0);
+        label10.setText("Node Attribute");
+        nodePanel.add(label10, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JLabel label11 = new JLabel();
         Font label11Font = this.$$$getFont$$$(null, Font.BOLD, 12, label11.getFont());
         if (label11Font != null) label11.setFont(label11Font);
-        label11.setText("Location info");
-        mainPanel.add(label11, new GridConstraints(4, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        label11.setText("Coordinates");
+        nodePanel.add(label11, new GridConstraints(2, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JLabel label12 = new JLabel();
-        Font label12Font = this.$$$getFont$$$(null, Font.BOLD, 14, label12.getFont());
+        Font label12Font = this.$$$getFont$$$(null, Font.BOLD, 12, label12.getFont());
         if (label12Font != null) label12.setFont(label12Font);
-        label12.setHorizontalAlignment(10);
-        label12.setHorizontalTextPosition(0);
-        label12.setText("Node Attribute");
-        mainPanel.add(label12, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        addAttributesButton = new JButton();
-        addAttributesButton.setText("Add attributes");
-        mainPanel.add(addAttributesButton, new GridConstraints(6, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        label12.setText("Location info");
+        nodePanel.add(label12, new GridConstraints(4, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        Font toggleButtonFont = this.$$$getFont$$$(null, Font.BOLD, -1, toggleButton.getFont());
+        if (toggleButtonFont != null) toggleButton.setFont(toggleButtonFont);
+        toggleButton.setHideActionText(false);
+        toggleButton.setHorizontalAlignment(2);
+        toggleButton.setText("Show Node Panel");
+        mainPanel.add(toggleButton, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     }
 
     /**
@@ -217,4 +247,10 @@ public class NodeAttributePanel extends JPanel {
         return mainPanel;
     }
 
+    private void createUIComponents() {
+        nodePanel = new JXCollapsiblePane();
+        ((JXCollapsiblePane) nodePanel).setAnimated(false);
+        ((JXCollapsiblePane) nodePanel).setCollapsed(true);
+        toggleButton = new JButton(nodePanel.getActionMap().get(JXCollapsiblePane.TOGGLE_ACTION));
+    }
 }
